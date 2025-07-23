2c5968b704087103a354e5610c1fe400354ca793
atikle <atikle.com@gmail.com>
2025-07-23 11:23:41 +0530

    Update

--- universal/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    // Ensure the path is correct relative to your HTML files
-    $("#navbar-placeholder").load("https://atikle.github.io/framework/navbar-1/index.html");
-});
-
-$(function () {
-    // Ensure the path is correct relative to your HTML files
-    $("#footer-placeholder").load("https://atikle.github.io/framework/footer-1/index.html");
-});
