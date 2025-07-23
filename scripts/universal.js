bd745314455683fec60f194c6cb7981cdf673af1
atikle <atikle.com@gmail.com>
2025-07-23 10:39:32 +0530

    Update

---
+++ scripts/universal.js
@@ -0,0 +1,11 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+$(function () {
+    // Ensure the path is correct relative to your HTML files
+    $("#navbar-placeholder").load("https://atikle.github.io/framework/navbar-1/index.html");
+});
+
+$(function () {
+    // Ensure the path is correct relative to your HTML files
+    $("#footer-placeholder").load("https://atikle.github.io/framework/footer-1/index.html");
+});
